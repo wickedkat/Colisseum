ef6ab87c883605fc8cf2d4ba8044f48a4499779b
Kat <kat.m.borowska@gmail.com>
2020-03-18 12:57:18 +0100

    add input testing

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Colisseum! How many stages of the Tournament do you wish to watch?");
+        System.out.println("Welcome to the Colisseum! How many stages of the Tournament do you wish to watch? (2-20)");
         String stagesUserInput = scanner.nextLine();
         int stages = Integer.parseInt(stagesUserInput);
+        if (stages <= 1 || stages > 20) {
+            throw new Exception("Incorrect number input");
+        } else {
 
-        Simulation simulation = new Simulation(stages);
-        simulation.simulateTournament();
+            Simulation simulation = new Simulation(stages);
+            simulation.simulateTournament();
 
+        }
     }
 }
